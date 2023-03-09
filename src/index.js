70f221702e294868be9c22b5f0f2fea90b137b6f
bsm718 <124943949+bsm718@users.noreply.github.com>
2023-03-08 18:03:59 -0800

    function for select added in js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
 function usaClock() {
   let laElement = document.querySelector("#los-angeles");
+  if (laElement) {
+    let laDate = laElement.querySelector(".date");
+    let laTime = laElement.querySelector(".time");
+    let laMoment = moment().tz("America/Los_Angeles");
 
-  let laDate = laElement.querySelector(".date");
-  let laTime = laElement.querySelector(".time");
-  let laMoment = moment().tz("America/Los_Angeles");
-
-  laDate.innerHTML = laMoment.format("dddd, MMMM D, YYYY");
-  laTime.innerHTML = laMoment.format("h:mm:ss [<small>]A[<small>]");
+    laDate.innerHTML = laMoment.format("ddd, MMMM D, YYYY");
+    laTime.innerHTML = laMoment.format("h:mm:ss [<small>]A[<small>]");
+  }
 
   let chicagoElement = document.querySelector("#chicago");
+  if (chicagoElement) {
+    let chicagoDate = chicagoElement.querySelector(".date");
+    let chicagoTime = chicagoElement.querySelector(".time");
+    let chicagoMoment = moment().tz("America/Chicago");
 
-  let chicagoDate = chicagoElement.querySelector(".date");
-  let chicagoTime = chicagoElement.querySelector(".time");
-  let chicagoMoment = moment().tz("America/Chicago");
-
-  chicagoDate.innerHTML = chicagoMoment.format("dddd, MMMM D, YYYY");
-  chicagoTime.innerHTML = chicagoMoment.format("h:mm:ss [<small>]A[<small>]");
+    chicagoDate.innerHTML = chicagoMoment.format("ddd, MMMM D, YYYY");
+    chicagoTime.innerHTML = chicagoMoment.format("h:mm:ss [<small>]A[<small>]");
+  }
 
   let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDate = newYorkElement.querySelector(".date");
+    let newYorkTime = newYorkElement.querySelector(".time");
+    let newYorkMoment = moment().tz("America/New_York");
 
-  let newYorkDate = newYorkElement.querySelector(".date");
-  let newYorkTime = newYorkElement.querySelector(".time");
-  let newYorkMoment = moment().tz("America/New_York");
-
-  newYorkDate.innerHTML = newYorkMoment.format("dddd, MMMM D, YYYY");
-  newYorkTime.innerHTML = newYorkMoment.format("h:mm:ss [<small>]A[<small>]");
+    newYorkDate.innerHTML = newYorkMoment.format("ddd, MMMM D, YYYY");
+    newYorkTime.innerHTML = newYorkMoment.format("h:mm:ss [<small>]A[<small>]");
+  }
+}
+function changeCity(event) {
+  let timezone = event.target.value;
+  let name = timezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(timezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+        <div>
+            <h2>${name} </h2>
+        </div>
+        <div>    
+            <div class="date">${cityTime.format("ddd, MMMM, D, YYYY")}</div>
+            <div class="time">${cityTime.format("h:mm:ss [<small>]A[<small>]")}
+        </div>
+        
+        <a href="/">Back to US Timezones</a>
+        
+    </div></div>`;
 }
 
 usaClock();
 setInterval(usaClock, 1000);
+
+let worldCitiesElement = document.querySelector("#worldCities");
+worldCitiesElement.addEventListener("change", changeCity);
