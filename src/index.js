49ddb0a153c37f8f424211bb87f743e245a8f979
bsm718 <124943949+bsm718@users.noreply.github.com>
2023-03-13 14:02:15 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function usaClock() {
     let laMoment = moment().tz("America/Los_Angeles");
 
     laDate.innerHTML = laMoment.format("ddd, MMMM D, YYYY");
-    laTime.innerHTML = laMoment.format("h:mm:ss [<small>]A[<small>]");
+    laTime.innerHTML = laMoment.format("h:mm:ss [<small>] A[<small>]");
   }
 
   let chicagoElement = document.querySelector("#chicago");
@@ -16,7 +16,9 @@ function usaClock() {
     let chicagoMoment = moment().tz("America/Chicago");
 
     chicagoDate.innerHTML = chicagoMoment.format("ddd, MMMM D, YYYY");
-    chicagoTime.innerHTML = chicagoMoment.format("h:mm:ss [<small>]A[<small>]");
+    chicagoTime.innerHTML = chicagoMoment.format(
+      "h:mm:ss [<small>] A[<small>]"
+    );
   }
 
   let newYorkElement = document.querySelector("#new-york");
@@ -26,34 +28,75 @@ function usaClock() {
     let newYorkMoment = moment().tz("America/New_York");
 
     newYorkDate.innerHTML = newYorkMoment.format("ddd, MMMM D, YYYY");
-    newYorkTime.innerHTML = newYorkMoment.format("h:mm:ss [<small>]A[<small>]");
+    newYorkTime.innerHTML = newYorkMoment.format(
+      "h:mm:ss [<small>] A[<small>]"
+    );
   }
 }
+
 function changeCity(event) {
   let timezone = event.target.value;
+  let country = document.querySelector(".country");
+  let flag = document.querySelector(".flag");
+
   if (timezone === "currentLocation") {
     timezone = moment.tz.guess();
+    country = "";
+    GMT = `GMT${moment().tz(timezone).format("Z")}`;
+    flag = "📍";
   }
+  if (timezone === "America/Caracas") {
+    country = "VE";
+    GMT = `GMT${moment().tz(timezone).format("Z")}`;
+    flag = "🇻🇪";
+  }
+  if (timezone === "Europe/London") {
+    country = "UK";
+    GMT = `GMT${moment().tz(timezone).format("Z")}`;
+    flag = "🇬🇧";
+  }
+  if (timezone === "Africa/Nairobi") {
+    country = "KE";
+    GMT = `GMT${moment().tz(timezone).format("Z")}`;
+    flag = "🇰🇪";
+  }
+  if (timezone === "Asia/Bangkok") {
+    country = "TH";
+    GMT = `GMT${moment().tz(timezone).format("Z")}`;
+    flag = "🇹🇭";
+  }
+
   let name = timezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(timezone);
-
   let citiesElement = document.querySelector("#cities");
+
   citiesElement.innerHTML = `<div class="city">
-        <div>
-            <h2>${name} </h2>
-        </div>
-        <div>    
-            <div class="date">${cityTime.format("ddd, MMMM, D, YYYY")}</div>
-            <div class="time">${cityTime.format("h:mm:ss [<small>]A[<small>]")}
-        </div>
-        
-        <a href="/">Back to US Timezones</a>
-        
-    </div></div>`;
+
+            <div>
+              <div>
+               <span class="flag">${flag}</span><span class="GMT">${GMT}</span>
+              </div>
+
+              <div class="worldDate">${cityTime.format(
+                "dddd, MMMM D, YYYY"
+              )}</div>
+
+            <div>
+              <span class="name">${name} </span><span class="country">${country}</span>
+            </div>
+       </div>
+       <div>
+            <div class="worldTime">${cityTime.format(
+              "h:mm:ss"
+            )}<small> ${cityTime.format("A")} <small>
+            </div></div></div>
+
+            
+        <a href="/" class="backToHomepage">Back to US Timezones</a></div>`;
 }
 
 usaClock();
 setInterval(usaClock, 1000);
 
-let worldCitiesElement = document.querySelector("#worldCities");
-worldCitiesElement.addEventListener("change", changeCity);
+let citiesElement = document.querySelector("#worldCities");
+citiesElement.addEventListener("change", changeCity);
